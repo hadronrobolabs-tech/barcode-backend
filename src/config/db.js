91dcdac78c6047d6e4ee2dfcc41ab1c4264fce43
hadronrobolabs-tech <hadronrobolabs@gmail.com>
2026-01-02 01:39:44 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,5 +1,9 @@
 const mysql = require("mysql2/promise");
 
+if (!process.env.MYSQL_URL) {
+  throw new Error("❌ MYSQL_URL is missing. Check Railway Variables or .env");
+}
+
 const pool = mysql.createPool(process.env.MYSQL_URL);
 
 (async () => {
@@ -7,7 +11,7 @@ const pool = mysql.createPool(process.env.MYSQL_URL);
     const conn = await pool.getConnection();
     await conn.ping();
     conn.release();
-    console.log("✅ MySQL Connected");
+    console.log("✅ Railway MySQL Connected");
   } catch (err) {
     console.error("❌ MySQL Connection Failed:", err.message);
   }
