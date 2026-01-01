69e24d55d63828ac23065428eeb7d3f898135d68
hadronrobolabs-tech <hadronrobolabs@gmail.com>
2026-01-02 02:02:50 +0530

    Add SSL configuration to database connection

--- src/config/db.js
+++ src/config/db.js
@@ -7,7 +7,8 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
-  connectionLimit: 10
+  connectionLimit: 10,
+  ssl: { rejectUnauthorized: false }
 });
 
 (async () => {
